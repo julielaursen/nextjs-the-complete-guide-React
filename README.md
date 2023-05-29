a8e771dd5e04edb2f169f4fd43d69517637a4ac9
julielaursen <julielaursen1@gmail.com>
2023-05-29 11:32:14 -0500

    create first component

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # nextjs-the-complete-guide-React
+
+Next JS the complete guide udemy course 02-React Refresher
+
+### To start:
+
+cd react-refresher
+npm start
---
+++ react-refresher/src/App.js
@@ -0,0 +1,12 @@
+import Todo from './components/Todo'
+
+function App() {
+  return (
+    <div>
+      <hi>My Todos</hi>
+      <Todo />
+    </div>
+  )
+}
+
+export default App
