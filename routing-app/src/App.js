8cb85c1af442698766d7785b569bf75175bf6dbb
julielaursen <julielaursen1@gmail.com>
2023-05-29 15:29:06 -0500

    update routes

--- routing-app/src/App.js
+++ routing-app/src/App.js
@@ -1,5 +1,24 @@
+import { Route, Switch } from 'react-router-dom'
+import AllMeetupsPage from './pages/AllMeetups'
+import Favorites from './pages/Favorites'
+import NewMeetupPage from './pages/NewMeetup'
+
 function App() {
-  return <div>hello world</div>
+  return (
+    <div>
+      <Switch>
+        <Route path="/" exact={true}>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage></NewMeetupPage>
+        </Route>
+        <Route path="/favorites">
+          <Favorites></Favorites>
+        </Route>
+      </Switch>
+    </div>
+  )
 }
 
 export default App
