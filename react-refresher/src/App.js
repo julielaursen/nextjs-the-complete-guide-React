5aaef56c74063ea0bffe69ef2187e091a1b6a65b
julielaursen <julielaursen1@gmail.com>
2023-05-29 11:54:30 -0500

    Working commit: static interface

--- react-refresher/src/App.js
+++ react-refresher/src/App.js
@@ -1,4 +1,6 @@
 import Todo from './components/Todo'
+import Modal from './components/Modal'
+import Backdrop from './components/Backdrop'
 
 function App() {
   return (
@@ -7,6 +9,8 @@ function App() {
       <Todo text="Learn react" />
       <Todo text="Master react" />
       <Todo text="Explore full react course" />
+      <Modal />
+      <Backdrop />
     </div>
   )
 }
