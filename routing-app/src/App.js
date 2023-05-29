e5d74c1f476b0d2d3613f8c4ea7c80be6854456c
julielaursen <julielaursen1@gmail.com>
2023-05-29 15:41:29 -0500

    add styling

--- routing-app/src/App.js
+++ routing-app/src/App.js
@@ -2,10 +2,12 @@ import { Route, Switch } from 'react-router-dom'
 import AllMeetupsPage from './pages/AllMeetups'
 import Favorites from './pages/Favorites'
 import NewMeetupPage from './pages/NewMeetup'
+import MainNavigation from './components/layout/MainNavigation'
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Switch>
         <Route path="/" exact={true}>
           <AllMeetupsPage />
