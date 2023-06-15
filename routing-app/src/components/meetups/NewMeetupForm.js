581a872c7c4e2a76a771ddf662c342851be928b7
julielaursen <julielaursen1@gmail.com>
2023-06-15 18:42:02 -0500

    add handler

--- routing-app/src/components/meetups/NewMeetupForm.js
+++ routing-app/src/components/meetups/NewMeetupForm.js
@@ -2,9 +2,10 @@ import Card from '../ui/Card'
 import classes from './NewMeetupForm.module.css'
 
 function NewMeetupForm() {
+  function submitHandler() {}
   return (
     <Card>
-      <form className={classes.form}>
+      <form className={classes.form} onSubmit={submitHandler}>
         <div className={classes.control}>
           <label htmlFor="title">Meetup Title</label>
           <input type="text" required id="title" />
