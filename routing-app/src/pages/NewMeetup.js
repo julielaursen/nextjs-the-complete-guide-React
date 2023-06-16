93e34f1f6d1995bf4a813b86a751af1a914dfc9c
julielaursen <julielaursen1@gmail.com>
2023-06-15 22:02:19 -0500

    update video 44

--- routing-app/src/pages/NewMeetup.js
+++ routing-app/src/pages/NewMeetup.js
@@ -1,10 +1,23 @@
 import NewMeetupForm from '../components/meetups/NewMeetupForm'
 
 function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    fetch(
+      'https://react-getting-started-7a021-default-rtdb.firebaseio.com/meetups.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(meetupData),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    )
+  }
+
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   )
 }
